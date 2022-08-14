851f2e59f72bed291cfb5098a51bb6e0d551862f
Matthew Templeton <matthew.templeton@cfa.harvard.edu>
2022-08-14 16:16:14 -0400

    Alembic setup, add .gitignore, del pycache  	new file:   .gitignore  	new file:   alembic.ini  	new file:   alembic/README  	new file:   alembic/env.py  	new file:   alembic/script.py.mako  	new file:   alembic/versions/f0123456789a_blank_init.py  	modified:   requirements.txt

---
+++ alembic/versions/f0123456789a_blank_init.py
@@ -0,0 +1,24 @@
+"""Blank init
+
+Revision ID: f0123456789a
+Revises:
+Create Date: 2019-10-03 13:17:50.925566
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f0123456789a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
