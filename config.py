273effee300da8d98bee36aa78dee5438ff9fcfc
Matthew Templeton <matthew.templeton@cfa.harvard.edu>
2022-10-13 12:54:58 -0400

    modified:   config.py  	modified:   requirements.txt

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
-HARVEST_BASE_DIR = '/proj/ads/abstracts/sources/Crossref2/'
+HARVEST_BASE_DIR = '/proj/adsx/ads_completeness/'
 
 HARVEST_LOG_DIR = HARVEST_BASE_DIR + '/UpdateAgent/'
-
-OUTPUT_MAP_FILENAME = 'doi_bibc_xref.map'
 
 CLASSIC_DOI_FILE = '/proj/ads/abstracts/config/links/DOI/all.links'
 CLASSIC_ALTBIBS = '/proj/ads/abstracts/config/bibcodes.list.alt'
