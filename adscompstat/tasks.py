8b3f173fb4136f773b7db14ed6e0242106ee30a6
Matthew Templeton <matthew.templeton@cfa.harvard.edu>
2023-09-16 14:58:32 -0400

    modified:   adscompstat/tasks.py

--- adscompstat/tasks.py
+++ adscompstat/tasks.py
@@ -18,20 +18,57 @@ app = app_module.ADSCompStatCelery("completeness-statistics-pipeline", proj_home
 logger = app.logger
 
 app.conf.CELERY_QUEUES = (
-    # Queue("parse-metafile", app.exchange, routing_key="parse-metafile"),
-    # Queue("add-emptyrecord", app.exchange, routing_key="add-emptyrecord"),
+    Queue("get-logfiles", app.exchange, routing_key="get-logfiles"),
+    Queue("parse-meta", app.exchange, routing_key="parse-meta"),
+    # Queue("match-classic", app.exchange, routing_key="match-classic"),
     # Queue("compute-stats", app.exchange, routing_key="compute-stats"),
     # Queue("output-metadata", app.exchange, routing_key="output-metadata"),
-    Queue("get-logfiles", app.exchange, routing_key="get-logfiles"),
 )
+
 
 @app.task(queue="get-logfiles")
 def task_process_logfile(infile):
+    batch_count = conf.get("RECORDS_PER_BATCH", 100)
     try: 
         files_to_process = utils.read_updateagent_log(infile)
+        batch = []
         for xmlFile in files_to_process:
             xmlFilePath = app.conf.get("HARVEST_BASE_DIR", "/") + xmlFile
-            logger.debug("I have a file name: %s" % xmlFilePath)
-            # task_process_metafile.delay(xmlFile)
+            batch.append(xmlFilePath)
+            if len(batch) > batch_count:
+                logger.debug("Calling task_parse_meta with batch '%s'" % batch)
+                task_parse_meta.delay(batch)
+                batch = []
+        if len(batch):
+            logger.debug("Calling task_parse_meta with batch '%s'" % batch)
+            task_parse_meta.delay(batch)
     except Exception as err:
         logger.error("Error processing logfile %s: %s" % (infile, err))
+
+
+@app.task(queue="parse-meta")
+def task_parse_meta(infile_batch):
+    try:
+        failures = []
+        batch_out = []
+        for infile in infile_batch:
+            try:
+                record = utils.parse_one_meta_xml(infile)
+                if record:
+                    with app.session_scope() as session:
+                        issn_list = record.get("publication", {}).get("ISSN", [])
+                        bibstem = None
+                        for issn in issn_list:
+                            if not bibstem:
+                                issnString = issn.get("issnString", None)
+                                if issnString:
+                                    bibstem = session.query(issn_bibstem.bibstem).filter(issn_bibstem.issn==issn).first()
+                            
+                      
+                else:
+                    failures.append({"file": infile, "status": "parser failed"})
+            except Exception as err:
+                failures.append({"file": infile, "status": "error: %s" % err}
+        
+    except Exception as err:
+        logger.error("Error processing record bundle: %s" % err)
