09d82ce8439458a5e1b54cd610551808eb2be3b8
Matthew Templeton <matthew.templeton@cfa.harvard.edu>
2024-01-29 09:50:50 -0500

    modified:   adscompstat/database.py

--- adscompstat/database.py
+++ adscompstat/database.py
@@ -12,10 +12,10 @@ from adscompstat.models import CompStatSummary as summary
 
 proj_home = os.path.realpath(os.path.join(os.path.dirname(__file__), "../"))
 app = app_module.ADSCompStatCelery(
-"completeness-statistics-pipeline",
-proj_home=proj_home,
-config=globals().get("config", {}),
-local_config=globals().get("local_config", {}),
+    "completeness-statistics-pipeline",
+    proj_home=proj_home,
+    config=globals().get("config", {}),
+    local_config=globals().get("local_config", {}),
 )
 
 class DBClearClassicException(Exception):
